663d6934c7c77c5c675928ab2432d2edd2a78fd7
Benson <56320827+GnubCarnival@users.noreply.github.com>
2023-05-28 12:26:48 -0400

    flush for next compare

--- EventCallback.cs
+++
@@ -1,8 +0,0 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: EventCallback
-// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
-
-
-public class EventCallback
-{
-}
--- IPunObservable.cs
+++
@@ -1,9 +0,0 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: IPunObservable
-// Assembly: Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
-
-
-public interface IPunObservable
-{
-  void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info);
-}
